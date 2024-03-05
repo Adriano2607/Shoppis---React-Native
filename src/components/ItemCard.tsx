398236239d44136a471bf30cf5d6bdbf7ec1c71e
Adriano2607 <rodrigues.adriano2607@gmail.com>
2024-03-05 00:57:43 -0300

    inicio

---
+++ src/components/ItemCard.tsx
@@ -0,0 +1,56 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { ProductDTO } from "../types/Products";
+
+interface Props {
+  product: ProductDTO;
+}
+
+const ItemCard = ({ product }: Props) => {
+  const navigation = useNavigation<any>();
+  
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate("Details")}>
+      <View style={styles.card}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.img}
+            source={{ uri: product.thumbnail }}
+          />
+        </View>
+        <View style={styles.text}>
+          <Text style={{fontSize:20,fontWeight:"700",textTransform:'capitalize'}}>{product.title}</Text>
+          <Text style={{fontStyle:'italic',fontSize:15}}>$ {product.price} </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ItemCard;
+
+const styles = StyleSheet.create({
+  card:{
+    backgroundColor:'white',
+    width:300,
+    height:280,
+    borderRadius:10,
+    marginVertical:10,
+    borderWidth:1,
+    borderColor:'silver'
+  },
+  imageContainer: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  img:{
+     width: '100%', 
+     height: 150,  
+  },
+  text:{
+    alignItems:'center',
+    marginTop:25,
+  }
+});
---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthRoutes } from "./AuthRoutes";
+import { AppRoutes } from "./AppRoutes";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
+
+export const Routes = () => {
+ // const { getToken, token } = useContext(UserContext);
+
+ // useEffect(() => {
+   // getToken();
+ // }, []);
+
+ const token = `dadqeawd`
+
+  return (
+    <NavigationContainer>
+      {token ? <AppRoutes /> : <AuthRoutes />}
+    </NavigationContainer>
+  );
+};
